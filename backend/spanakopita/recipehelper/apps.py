a846c6fabc85e4065efe84f06e9d991a3fe493fc
Tyler Nagamine <tylernagamine@gmail.com>
2016-12-29 23:51:10 -0800

    Initial commit.  Implemented basic timer model and playing with serializer settings now.

---
+++ backend/spanakopita/recipehelper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RecipehelperConfig(AppConfig):
+    name = 'recipehelper'
---
+++ backend/spanakopita/recipehelper/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Timer(models.Model):
+    STOPWATCH = 'sw'
+    TIMER = 't'
+
+    TIMER_CHOICES = (
+        (TIMER, 'timer'),
+        (STOPWATCH, 'stopwatch')
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    duration = models.IntegerField(default=0)
+    title = models.CharField(max_length=100, blank=True, null=False)
+    type = models.CharField(max_length=2, choices=TIMER_CHOICES, default=STOPWATCH)
+    # owner = models.ForeignKey()
