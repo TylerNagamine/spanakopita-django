c7ecbf571ec89dffcf6588deb046cfea17c4d6fd
Tyler Nagamine <tylernagamine@gmail.com>
2016-12-30 03:52:12 -0800

    Timer api is very concise now

--- backend/spanakopita/recipehelper/views.py
+++ backend/spanakopita/recipehelper/views.py
@@ -1,44 +1,11 @@
 from recipehelper.models import Timer
 from recipehelper.serializers import TimerSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
-class TimerList(APIView):
-    def get(self, request, format=None):
-        timers = Timer.objects.all()
-        serializer = TimerSerializer(timers, many=True)
-        return Response(serializer.data)
+class TimerList(generics.ListCreateAPIView):
+    queryset = Timer.objects.all()
+    serializer_class = TimerSerializer
 
-    def post(self, request, format=None):
-        serializer = TimerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class TimerDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Timer.objects.get(pk=pk)
-        except Timer.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk, format=None):
-        timer = self.get_object(pk)
-        serializer = TimerSerializer(timer)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        timer = self.get_object(pk)
-        serializer = TimerSerializer(timer, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        timer = self.get_object(pk)
-        timer.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class TimerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Timer.objects.all()
+    serializer_class = TimerSerializer
